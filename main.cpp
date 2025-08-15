8f2f193fee7e25e8be1a47239b3fbce4bc49d2f2
francesco <francesco.zaccherelli@edu.unifi.it>
2025-08-15 20:23:14 +0200

    Creata la classe Timer con metodi start stop reset.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 class Data {
 private:
@@ -45,10 +47,10 @@ public:
 class Tempo{
     private:
         int ora;
-        int minuti;
-        int secondi;
+        int minuto;
+        int secondo;
     public:
-        Tempo (int ora, int minuto, int secondo) : ora(ora), minuti(minuto), secondi(secondo) {
+        Tempo (int ora, int minuto, int secondo) : ora(ora), minuto(minuto), secondo(secondo) {
             if (ora < 0 || ora > 23) {
                 std::cout << "Errore: ora non valida" << std::endl;
             }
@@ -61,8 +63,66 @@ class Tempo{
         }
         void avanza(){};
    };
+class Timer {
+private:
+    int ora;
+    int minuto;
+    int secondo;
+    bool avviato;
+    int secondiTotale;
+public:
+    Timer (int ora, int minuto, int secondo) : ora(ora), minuto(minuto), secondo(secondo), avviato(false) {
+        if (ora < 0 || ora > 23) {
+            std::cout << "Errore: ora non valida" << std::endl;
+        }
+        if (minuto < 0 || minuto > 59) {
+            std::cout << "Errore: minuto non valido" << std::endl;
+        }
+        if (secondo < 0 || secondo > 59) {
+            std::cout << "Errore: secondo non valido" << std::endl;
+        }
+        secondiTotale = secondo + minuto*60 + ora*3600;
+    }
+
+        void start() {
+        std::chrono::seconds durataTotale(secondiTotale);
+        auto start = std::chrono::steady_clock::now();
+        if (avviato != true) {
+            avviato = true;
+        }
+        while (avviato == true) {
+            auto sOra = std::chrono::steady_clock::now();
+            auto sPassati = duration_cast<std::chrono::seconds>(sOra - start);
+            auto sRimanenti = durataTotale - sPassati;
+
+            if (sRimanenti.count() <= 0) {
+                std::cout <<"Tempo scaduto"<<std::endl;
+                break;
+            }
+            std::cout<<sRimanenti.count()<< " secondi"<<std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+
+
+    }
+        void stop() {
+        avviato = false;
+        std::cout<<"Tempo fermato"<<std::endl;
+    };
+        void reset() {
+            avviato = false;
+            secondiTotale = 0;
+            std::cout<<"Tempo resettato"<<std::endl;
+        };
+};
 
 int main() {
     Data data(14, 8, 2025);
+    Timer timer1(0, 1, 0);
+    timer1.start();
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    timer1.stop();
+
 
 }
