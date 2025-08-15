9d6a943a9570e4ab7a36377ac53bfee44f3e9405
francesco <francesco.zaccherelli@edu.unifi.it>
2025-08-15 19:32:29 +0200

    Creata la classe Tempo.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 class Data {
-public:
+private:
     int giorno;
     int mese;
     int anno;
-
+public:
     Data (int giorno, int mese, int anno) : giorno(giorno), mese(mese), anno(anno) {
         if (anno < 1900 || anno > 2100) {
             std::cout << "Errore: anno non valido" << std::endl;
@@ -41,6 +41,26 @@ public:
     }
 
 };
+
+class Tempo{
+    private:
+        int ora;
+        int minuti;
+        int secondi;
+    public:
+        Tempo (int ora, int minuto, int secondo) : ora(ora), minuti(minuto), secondi(secondo) {
+            if (ora < 0 || ora > 23) {
+                std::cout << "Errore: ora non valida" << std::endl;
+            }
+            if (minuto < 0 || minuto > 59) {
+                std::cout << "Errore: minuto non valido" << std::endl;
+            }
+            if (secondo < 0 || secondo > 59) {
+                std::cout << "Errore: secondo non valido" << std::endl;
+            }
+        }
+        void avanza(){};
+   };
 
 int main() {
     Data data(14, 8, 2025);
