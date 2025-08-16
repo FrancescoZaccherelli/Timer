1ca95d26288d59c7ddaaac7e140a312867e32319
francesco <francesco.zaccherelli@edu.unifi.it>
2025-08-16 16:49:24 +0200

    Aggiunto al cmake qt5 per la gui. Prime prove d'uso

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 #include "Data.h"
 #include "Tempo.h"
 #include "Timer.h"
-
-int main() {
-    Data d(31, 12, 2025);
-    Tempo t(23, 59, 55, &d); // legato alla data
-
-    d.stampaData();   // stampa data iniziale
-    t.avanza();       // quando scatta mezzanotte vedrai nuova data
-
-    return 0;
-
-    return 0;
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QTimer>
+#include <QString>
 
 
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("App");
+    window.resize(200, 100);
+    window.show();
+
+    QLabel label(&window);
+    label.setText("00:00:00");
+    label.move(100, 50);
+    label.show();
+
+    return app.exec();
 }
